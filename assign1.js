3b04925bbc1fe317b575fd595aa3f3b5334d3a48
snowpoff <fayore@gmail.com>
2017-02-02 17:22:47 +0000

    second commit

--- assign1.js
+++ assign1.js
@@ -1,8 +1,20 @@
 //create a function that passes a value to an array. both the array and value should be passed in as parameters, function arrPush (array, value)
 
 function arrPush (array, value){
-    let array = []
     array.push(value);
 }
 
-// ? I don't think I understand how to do this
+
+
+/* following code just to test arrPush()
+
+let animalArray = []
+animalArray.push("bird", "dog", "monkey")
+
+console.log(animalArray) // [ 'bird', 'dog', 'monkey' ]
+
+arrPush(animalArray, "flamingo")
+
+console.log(animalArray) // [ 'bird', 'dog', 'monkey', 'flamingo' ]
+
+*/
